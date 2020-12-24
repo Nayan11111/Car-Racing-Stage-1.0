875ec4a65c4fb95ad7ed8738c12d9f428abd8f19
Nayan <nkaul0515@icloud.com>
2020-12-24 18:21:58 -0500

    Database Game

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ var playerCount;
 var database;
 
 var form, player, game;
-
+var allPlayers;
 
 function setup(){
   canvas = createCanvas(400,400);
@@ -18,4 +18,13 @@ function setup(){
 
 
 function draw(){
+  if (playerCount===4){
+    // game.update();
+    gameState = 1;
+    game.update(gameState);
+  }
+  if (gameState===1){
+    clear();
+    game.play();
+  }
 }
